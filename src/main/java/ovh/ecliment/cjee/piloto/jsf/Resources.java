69eb88ce9f03c645530cd0f0c13132ed35bb96d9
Enric Climent <enric612@gmail.com>
2018-03-26 10:58:17 +0200

    App inicial (index-editPartit)

--- src/main/java/ovh/ecliment/cjee/piloto/jsf/Resources.java
+++ src/main/java/ovh/ecliment/cjee/piloto/jsf/Resources.java
@@ -13,10 +13,14 @@ import javax.enterprise.inject.spi.InjectionPoint;
 /**
  *
  * @author ecliment
+ * L'anotació dependent indica que el recurs (classe) dependra del context en
+ * el que estiga invocat
  */
 @Dependent
 public class Resources {
 
+    // Produces es gasta per a generar un objecte que pot ser injectat 
+    // Este objecte no es un bean. 
     @Produces
     public Logger getLogger(InjectionPoint p) {
         return Logger.getLogger(p.getMember().getDeclaringClass().getName());
